e4c4edddff65f96507feb1f811daafdc251837e3
lizshu <elshumake@gmail.com>
2020-11-05 22:19:30 -0600

    changed location of test and added comment about cookies

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -28,18 +28,18 @@ final class HttpRequest implements Runnable
         InputStream inStream = socket.getInputStream();
         DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
 
-        // Set up input stream filters.
-        BufferedReader br = new BufferedReader(new InputStreamReader(inStream));//reads the input data
+        //reads input data
+        BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
 
-        // Get request line of HTTP request message.
-        String requestLine = br.readLine();// get /path/file.html version of http
+        // Get request line of HTTP request message. (/path/file.html version of http)
+        String requestLine = br.readLine();
 
         // Extract filename from the request line.
-        StringTokenizer tokens = new StringTokenizer(requestLine);// this is a input method with deliminators
+        StringTokenizer tokens = new StringTokenizer(requestLine);
         tokens.nextToken(); // skip over the method, which should be "GET"
         String fileName = tokens.nextToken();
 
-        // Prepend a "." so that file request is within the current directory.
+        //add "." to declare that file is in current directory.
         fileName = "." + fileName;
 
         //Open the requested file.
@@ -61,6 +61,17 @@ final class HttpRequest implements Runnable
             statusLine = "HTTP/1.1 404 Not Found" + CRLF; //common error message
         }
 
+        /* I really struggled to implement cookies, so I ended up deleting what I had.
+        This is where the cookie information would go.
+            There would be a setPath that required "/els141" to be within the provided URL
+            There would be a setMaxTime=27000 that would tell when to kill the cookie
+            The cookie would be called numVisited and the value for that cookie would be iterated whenever a valid URL is called
+            Then, whenever https://eecslab-10:50064/els141/visit.html was called, the html file would be run and we'd add a
+                reference to the value within the cookie which would be displayed on the page
+            Which would result in a page that contained the text on the visit.html page followed by the number of times
+                that valid URLs have been visited
+         */
+
         //Send the status line.
         outStream.writeBytes(statusLine);
         //Send a blank line to indicate the end of the header lines.
@@ -73,6 +84,7 @@ final class HttpRequest implements Runnable
         sendBytes(fis, outStream);
         fis.close();
 
+        //Added to give more information when call fails/succeeds
         System.out.println("*****");
         System.out.println(fileName);//print out file request to console
         System.out.println("*****");
